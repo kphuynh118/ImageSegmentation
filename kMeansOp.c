c00925f9ba797d26452d9be149fba846ebe3be0b
dulloa21 <158112708+dulloa21@users.noreply.github.com>
2024-04-12 12:38:27 -0700

    Add files via upload

--- kMeansOp.c
+++ kMeansOp.c
@@ -1,24 +1,24 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h>
+#include <omp.h>
+#include <string.h>
 #include <opencv2/opencv.hpp>
 #include <iostream>
 #include <chrono>
 
-//OpenCV Version: 3.4.6
-//To compile: g++ -Wall -g -o readimage readimage.c `pkg-config --cflags --libs opencv`
-//then ./readimage
-//it should print out 5x5 matrix of the top-left
-int kmeansSerial(cv::Mat &image,cv::Mat &original);
 int openImage(cv::Mat &image, std::string imagePath);
+int kmeansOp(cv::Mat &image, cv::Mat &original);
+int threadCount; 
 
-int main (void){
-    cv::Mat image128, original128, image256, original256, image512, original512; 
+int main(int argc, char *argv[]){
+    threadCount = strtol(argv[1], NULL, 10);
+    
+     cv::Mat image128, original128, image256, original256, image512, original512; 
     /////////// 128 /////////////
     openImage(image128, "/home/605/ulloa/project/camera128.jpg");
     image128.copyTo(original128);
-    kmeansSerial(image128, original128); 
-
-    std::string outputPath128 = "/home/605/ulloa/project/camera128kMeans_Serial.jpg";
+    kmeansOp(image128, original128);
+    std::string outputPath128 = "/home/605/ulloa/project/camera128kMeans_OP.jpg";
     if(cv::imwrite(outputPath128,image128)){
         std::cout << "Segmented 128x128 image is succesfully saved to " << outputPath128 << "\n"<<std::endl;
     } else{
@@ -28,21 +28,20 @@ int main (void){
     ///////////// 256 //////////
     openImage(image256, "/home/605/ulloa/project/camera256.jpg");
     image256.copyTo(original256);
-    kmeansSerial(image256, original256); 
+    kmeansOp(image256, original256);
 
-    std::string outputPath256 = "/home/605/ulloa/project/camera256kMeans_Serial.jpg";
+    std::string outputPath256 = "/home/605/ulloa/project/camera256kMeans_OP.jpg";
     if(cv::imwrite(outputPath256,image256)){
         std::cout << "Segmented 256x256 image is succesfully saved to " << outputPath256 << "\n"<<std::endl;
     } else{
         std::cerr << "Failed to save the segmented image.\n" << std::endl;
     }
-
     //////////// 512 ////////////
     openImage(image512, "/home/605/ulloa/project/camera512.jpg");
     image512.copyTo(original512);
-    kmeansSerial(image512, original512); 
+    kmeansOp(image512, original512);
 
-    std::string outputPath512 = "/home/605/ulloa/project/camera512kMeans_Serial.jpg";
+    std::string outputPath512 = "/home/605/ulloa/project/camera512kMeans_OP.jpg";
     if(cv::imwrite(outputPath512,image512)){
         std::cout << "Segmented 512x512 image is succesfully saved to " << outputPath512 << "\n"<<std::endl;
     } else{
@@ -50,6 +49,7 @@ int main (void){
     }
     return 0; 
 }
+
 int openImage(cv::Mat &image, std::string imagePath){
 
     //std::string imagePath = "/home/605/ulloa/project/camera128.jpg"; 
@@ -81,7 +81,8 @@ int openImage(cv::Mat &image, std::string imagePath){
     // }
     return 0; 
 }
-int kmeansSerial(cv::Mat &image, cv::Mat &original){  
+
+int kmeansOp(cv::Mat &image, cv::Mat &original){  
     auto start = std::chrono::high_resolution_clock::now(); 
     int rows = image.rows; 
     int cols = image.cols; 
@@ -89,12 +90,14 @@ int kmeansSerial(cv::Mat &image, cv::Mat &original){
     int iter = 0; 
     int c1 = (rand() % 256);
     int c2 = (rand() % 256);
-    double d1, d2 ;
+    double d1 = 0, d2 = 0;
     int c1Sum, c2Sum ;
-    int i, j; 
+    int i,j; 
     while (iter < 1000){
         c1Sum = 0, c2Sum = 0; 
         color1 = 0, color2 = 0; 
+        #pragma omp parallel for num_threads(threadCount)\
+            reduction(+:c1Sum,c2Sum,color1,color2) private(i,j,d1,d2) shared(c1, c2, cols)
         for (i = 0; i < rows; i++){
             for (j = 0; j < cols; j++){
                 d1 = abs(original.at<uchar>(i, j) - c1);
@@ -105,7 +108,9 @@ int kmeansSerial(cv::Mat &image, cv::Mat &original){
                     image.at<uchar>(i, j) = 1; 
                 }
                 else{
+                    //#pragma omp critical
                     c2Sum++; 
+                    //#pragma omp critical
                     color2 += original.at<uchar>(i, j);
                     image.at<uchar>(i, j) = 2; 
                 }
@@ -133,7 +138,8 @@ int kmeansSerial(cv::Mat &image, cv::Mat &original){
         color1 = 255; 
         color2 = 0; 
     }
-
+    #pragma omp parallel for num_threads(threadCount)\
+        private(i,j)
     for(i = 0; i < rows; i++){
         for(j = 0; j <cols; j++){
             if (image.at<uchar>(i, j) == 1){
@@ -144,9 +150,10 @@ int kmeansSerial(cv::Mat &image, cv::Mat &original){
             }
         }
     }
+    
     auto end = std::chrono::high_resolution_clock::now(); 
 
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "Serial code takes " << elapsed.count() << " seconds to segment a "<< rows <<"x" << cols <<" camera man image\n";
+    std::cout << "Parallel code takes " << elapsed.count() << " seconds to segment a "<< rows <<"x" << cols <<" camera man image\n";
     return 0; 
 }
