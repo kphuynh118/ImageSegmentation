9750f1cc77b890284188b1b9f3ece7f09d1f549d
Luna Huynh <144747523+kphuynh118@users.noreply.github.com>
2024-08-20 11:08:12 -0700

    Delete checkOpenCVversion.cpp

--- checkOpenCVversion.cpp
+++
@@ -1,7 +0,0 @@
-#include <opencv2/core/core.hpp>
-#include <iostream>
-//to compile: g++ checkOpenCVversion.cpp -o checkOpenCVversion `pkg-config --cflags --libs opencv`
-int main() {
-    std::cout << "OpenCV Version: " << CV_VERSION << std::endl;
-    return 0;
-}
